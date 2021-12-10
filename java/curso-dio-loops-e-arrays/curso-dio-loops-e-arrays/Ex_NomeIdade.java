8b6d293bf12f3000902d27780f60ddf30e59879f
Ruths2 <ruth09@yahoo.com>
2021-12-10 01:09:46 -0300

    Cria curso de loops e arrays

---
+++ java/curso-dio-loops-e-arrays/curso-dio-loops-e-arrays/Ex_NomeIdade.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+//Exercícios curso: DIO-Estruturas de Repetição e Arrays em Java.
+
+/*
+ Faça um programa que leia conjuntos de dois valores,
+o primeiro representando o nome do aluno e o segundo representando a sua idade.
+(Pare o programa inserindo o valor 0 no campo nome)
+*/
+
+public class Ex_NomeIdade{
+	public static void main (String [ ] args) {
+		Scanner scan = new Scanner (System.in);
+		
+		String nome;
+		int idade;
+		
+		while(true){
+			System.out.println("__________________________________");
+			System.out.println("Digite nome: ");
+			nome = scan.next();
+			if (nome.equals("0")) break;
+			
+			System.out.println("Digite idade: ");
+			idade = scan.nextInt();
+			}
+		
+		System.out.println("__________________________________");
+		System.out.println("Programa encerrado com sucesso!");
+	}
+}
